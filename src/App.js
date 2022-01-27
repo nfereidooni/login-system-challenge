275413cbd0e64a05878b2a901e76052d7e81a2a0
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-27 17:27:18 -0500

    added login files and updated routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "bootstrap/dist/css/bootstrap.css"
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-          <SignUp/>
+          <Switch>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
           <Footer />
+          
     </Router>
   );
 }
